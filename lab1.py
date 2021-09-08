378ead9d9b538a465aecc7530a4c397bfa996ee3
Zoey Pu <zoeypu98@gmail.com>
2021-09-08 17:41:23 -0600

    download and print

--- lab1.py
+++ lab1.py
@@ -4,5 +4,7 @@ print(requests.__version__)
 
 #get Google homepage
 response = requests.get('http://www.google.com/')
-print (response.status_code)
-print (response.content)
+#print (response.status_code)
+
+r = requests.get('https://raw.githubusercontent.com/zoeypu98/C404Lab/main/lab1.py')
+print(r.text)
